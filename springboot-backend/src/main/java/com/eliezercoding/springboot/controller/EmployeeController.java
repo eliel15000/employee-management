4e3a15f51c79bfe47f7439453bf9e27f2dfadda9
eliel15000 <eliezercoding@gmail.com>
2022-12-19 23:27:48 -0500

    backend controller

---
+++ springboot-backend/src/main/java/com/eliezercoding/springboot/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.eliezercoding.springboot.controller;
+
+import com.eliezercoding.springboot.model.Employee;
+import com.eliezercoding.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
