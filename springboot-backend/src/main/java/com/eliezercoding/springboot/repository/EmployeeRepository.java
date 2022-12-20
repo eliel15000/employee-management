92804834f13c9bfb15b07483e52a1aacec5ceb9d
eliel15000 <eliezercoding@gmail.com>
2022-12-19 23:16:38 -0500

    backend repository

---
+++ springboot-backend/src/main/java/com/eliezercoding/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.eliezercoding.springboot.repository;
+
+import com.eliezercoding.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
