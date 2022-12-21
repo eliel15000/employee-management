9e7779eeb7fba8b311714035c57b5c841ca2927a
eliel15000 <eliezercoding@gmail.com>
2022-12-20 21:58:48 -0500

    frontend routing

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListEmployee from "./components/ListEmployee";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -6,12 +7,17 @@ import Footer from "./components/Footer";
 function App() {
   return (
     // <div className="App">
-    <div>
-      <Header />
-      <div className="container">
-        <ListEmployee />
-      </div>
-      <Footer />
+    <div className="App">
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" exact element={<ListEmployee />} />
+            <Route path="/employees" element={<ListEmployee />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
