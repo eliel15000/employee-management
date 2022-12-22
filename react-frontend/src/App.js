e646ad9201d1279dd6465c8aa59cbffb68c141bb
eliel15000 <eliezercoding@gmail.com>
2022-12-21 23:36:29 -0500

    backend deleteEmployee

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -4,7 +4,6 @@ import ListEmployee from "./components/ListEmployee";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CreateEmployee from "./components/CreateEmployee";
-import UpdateEmployee from "./components/UpdateEmployee";
 
 function App() {
   return (
